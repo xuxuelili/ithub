9a926b49128ef7be39ea9b4f55e4c9c5666bd9e1
xuxuelili <2211903076@qq.com>
2018-06-30 21:34:25 +0800

    添加话题-客户端

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,5 +1,7 @@
 const categoryModel = require('../models/category');
+const topicModel = require('../models/topic');
 
+//显示添加话题的页面
 exports.showCreate = (req,res) => {
     categoryModel.getAll((err, categories) => {
         res.render('topic/create.html',{
@@ -9,8 +11,36 @@ exports.showCreate = (req,res) => {
     
 };
 
+//处理添加话题
 exports.handleCreate = (req,res) => {
-    res.render();
+    //判断session
+    if (!req.session.user) {
+        res.json({
+            code: 403,
+            msg: '登录过期,请先登录'
+        })
+    }
+    req.body.userId = req.session.id;
+    req.body.createdAt = new Date();
+    topicModel.createTopic(req.body, (err, isOK) => {
+        if (err) {
+            res.json({
+                code: 500,
+                msg: '服务器内部错误'
+            })
+        }
+        if (isOK) {
+            res.json({
+                code: 200,
+                msg: '添加成功'
+            })
+        } else {
+            res.json({
+                code: 401,
+                msg: '添加失败'
+            })
+        }
+    })
 };
 
 exports.showTopic = (req,res) => {
